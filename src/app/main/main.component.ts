7fe245f127447487b2a1eff5c3c069310cc8580d
itsmesiddique22 <itsmesiddique22@gmail.com>
2018-02-21 22:42:08 +0530

    video auto play mobile angular

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,18 +1,20 @@
-import { Component, OnInit, AfterContentInit } from '@angular/core';
+import { Component, OnInit, AfterContentInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
+
 export class MainComponent implements OnInit {
+  @ViewChild('videoPlayer') videoplayer: any;
 
   constructor() { }
 test = function(){
   alert('hi');
 }
   ngOnInit() {
-    
+    this.videoplayer.nativeElement.play();
    // this.test();
   }
   ngAfterContentInit() {
