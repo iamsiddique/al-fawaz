7bc7c396660a0ee1e637ab45130e29e93f6dc390
iamviqar <iamviqar@gmail.com>
2018-03-26 15:07:33 +0530

    Revert "adding investment component"
    This reverts commit b3bf1ae1c9125361f0d307a81a10f1953cd8f6af.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,16 +4,13 @@ import { MainComponent }      from './main/main.component';
 import { AboutComponent }      from './about/about.component';
 import { ContactComponent }      from './contact/contact.component';
 import { ProductDetailComponent }      from './product-detail/product-detail.component';
-import { InvestmentCompComponent } from './investment-comp/investment-comp.component';
 
 const routes: Routes = [
   { path: '', component: MainComponent },  
   { path: 'about-us', component: AboutComponent },  
   { path: 'contact-us', component: ContactComponent }, 
-  { path: 'travel-details', component: ProductDetailComponent },
-  { path: 'investment', component: InvestmentCompComponent },  
-  { path: '', redirectTo: '/', pathMatch: 'full' },
-
+  { path: 'travel-details', component: ProductDetailComponent },  
+  { path: '', redirectTo: '/', pathMatch: 'full' }
 ];
 
 @NgModule({
